6f0f79b63dd66c9c181b15c2e63c1019bbf4b15d
Srikant Sahoo <srikant.sahoo@maersk.com>
2021-10-18 04:23:34 +0530

    :fire: Added our journey, video to gameplay
    Signed-off-by: Srikant Sahoo <srikant.sahoo@maersk.com>

--- src/containers/Gameplay/index.js
+++ src/containers/Gameplay/index.js
@@ -2,6 +2,7 @@ import { Flex } from "../../components/Container";
 import Theme from "../../Utils/theme";
 import Line from "../../assets/svgs/indicator_line.png";
 import { Headline } from "../../components/Headline";
+import { CustomLink } from "../../components/Link";
 export const Gameplay = () => {
   const styles = {
     activeCard: {
@@ -10,6 +11,7 @@ export const Gameplay = () => {
       width: "70vw",
       height: "70.5vh",
       margin: `${Theme.SPACING(10)} 0 0 0`,
+      overflow: "hidden",
     },
     flex: {
       margin: 0,
@@ -31,6 +33,26 @@ export const Gameplay = () => {
       marginBottom: Theme.SPACING(-42),
       zIndex: 1,
     },
+    video: {
+      width: "100%",
+      height: "auto",
+    },
+    watchOnText: {
+      position: "absolute",
+      marginTop: Theme.SPACING(-100),
+      marginLeft: Theme.SPACING(100),
+    },
+    userAccount: {
+      borderRadius: "499px",
+      height: Theme.SPACING(50),
+      background: Theme.COLORS.gradient.gradient_1,
+      border: `${Theme.COLORS.colors.color_1} solid 1px`,
+      width: Theme.SPACING(50),
+      marginRight: Theme.SPACING(10),
+    },
+    streamerName: {
+      color: Theme.COLORS.colors.color_1,
+    },
   };
   return (
     <Flex style={styles.flex}>
@@ -41,7 +63,34 @@ export const Gameplay = () => {
         </div>
       </Flex>
       <Flex>
-        <div style={styles.activeCard} className="gameplayVideo"></div>
+        <div>
+          <div style={styles.activeCard} className="gameplayVideo">
+            <video
+              style={styles.video}
+              className="video"
+              crossOrigin
+              playsInline
+              // autoPlay
+              muted
+              // controls={true}
+              poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
+              src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4">
+              <source
+                src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4"
+                type="video/mp4"
+              />
+            </video>
+          </div>
+          <div style={styles.watchOnText}>
+            <Flex>
+              <div style={styles.userAccount}> </div>
+              <div>
+                <div style={styles.streamerName}>Yoytube Gamer</div>
+                <CustomLink text="Stream on Youtube" href="#"></CustomLink>
+              </div>
+            </Flex>
+          </div>
+        </div>
         <Flex flexFlow="column wrap">
           <div style={styles.card} className="gameplayVideo"></div>
           <div style={styles.card} className="gameplayVideo"></div>
--- src/containers/Journey/index.js
+++ src/containers/Journey/index.js
@@ -4,6 +4,21 @@ import Line from "../../assets/svgs/indicator_line.png";
 import { Headline } from "../../components/Headline";
 import { Pxtorem } from "../../Utils/Constants";
 import Background from "../../assets/svgs/journey.png";
+
+const Box = ({ text }) => {
+  const styles = {
+    box: {
+      padding: `${Theme.SPACING(10)} ${Theme.SPACING(20)}`,
+      border: `${Theme.COLORS.colors.color_1} solid 1px`,
+    },
+  };
+  return (
+    <div style={styles.box} className="journeyBox">
+      {text}
+    </div>
+  );
+};
+
 export const OurJourney = () => {
   const styles = {
     flex: {
@@ -19,9 +34,66 @@ export const OurJourney = () => {
       height: "54rem",
     },
   };
+  const JSON = [
+    {
+      date: "June 2020",
+      topic: "Development started",
+    },
+    {
+      date: "July 2020",
+      topic: "Team formation",
+    },
+    {
+      date: "October 2020",
+      topic: "Beta testing started",
+    },
+    {
+      date: "1st November 2020",
+      topic: "Pre-registration started",
+    },
+    {
+      date: "16th November 2020",
+      topic: "Launch on Playstore",
+    },
+    {
+      date: "20th November 2020",
+      topic: "1k+ downloads",
+    },
+    {
+      date: "6th December 2020",
+      topic: "5k+ downloads",
+    },
+    {
+      date: "15th December 2020",
+      topic: "TMini Update release",
+    },
+    {
+      date: "16th December 2020",
+      topic: "10k+ downloads",
+    },
+    {
+      date: "21st January 2021",
+      topic: "50k+ downloads",
+    },
+    {
+      date: "7th March 2021",
+      topic: "100k+ downloads",
+    },
+    {
+      date: "15th May 2021",
+      topic: "Mega Update 2.0 release",
+    },
+    {
+      date: "July 2020",
+      topic: "Team formation",
+    },
+  ];
   return (
     <Flex style={styles.background}>
       <Headline text="OUR JOURNEY" />
+      {JSON.map((item) => {
+        return <Box text={item.topic} />;
+      })}
     </Flex>
   );
 };
